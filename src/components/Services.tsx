c5d90f4a26d79d53ce55d1b029759dda47b106be
Morsi <136155900+Mahmoud-Morsi0@users.noreply.github.com>
2025-07-26 20:06:12 +0300

    Refactor Services component to replace Link with a motion button for improved user interaction. Enhance call-to-action section with animation effects and adjust layout for better alignment and visual appeal.

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -4,11 +4,13 @@ import {useTranslations} from 'next-intl';
 import { useLocale } from 'next-intl';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
+import { motion } from 'framer-motion';
 
 export default function Services() {
   const t = useTranslations();
   const locale = useLocale();
-
+  const router = useRouter();
   const services = [
     {
       key: 'corporate',
@@ -85,19 +87,37 @@ export default function Services() {
       </section>
 
       {/* Call to Action */}
-      <section className="bg-pure-mint py-16">
-        <div className="container mx-auto px-6 text-center">
+      <section className="bg-pure-mint py-16 text-center" >
+        <div className="container mx-auto px-6 text-center flex flex-col items-center justify-center">
           <h2 className="text-4xl font-darker-grotesque font-bold text-cape-cod mb-6">
             {t('services.cta.title')}
           </h2>
           <p className="text-xl text-cape-cod mb-8 max-w-2xl mx-auto">
             {t('services.cta.description')}
           </p>
-          <Link href={`/${locale}/contact`}>
-            <Button className="bg-cape-cod text-pure-white px-8 py-4 rounded-full text-lg font-semibold hover:bg-obsidian transition-colors duration-300">
-              {t('services.cta.button')}
-            </Button>
-          </Link>
+
+          <motion.button
+            onClick={() => router.push('/contact')}
+            className="px-4 py-2 bg-cyan-950 text-cyan-50 dark:text-cyan-100 dark:bg-cyan-950 border-cyan-800 dark:border-cyan-100 rounded-full flex items-center hover:bg-obsidian hover:text-pure-white transition-colors font-mona-sans hover:border-cyan-100 cursor-pointer border"
+            animate={{
+              y: [0, -10, 0]
+            }}
+            transition={{
+              duration: 1.5,
+              repeat: Infinity,
+              ease: "easeInOut",
+              repeatType: "loop"
+            }}
+            whileHover={{
+              scale: 1.05,
+              y: -5
+            }}
+            whileTap={{
+              scale: 0.95
+            }}
+          >
+            {t('services.cta.button')} <span className="ml-2">▶</span>
+          </motion.button>
         </div>
       </section>
     </div>
