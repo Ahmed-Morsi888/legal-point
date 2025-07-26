55d3865982badfe2a8f0c0f086c157239b3d5903
Morsi <136155900+Mahmoud-Morsi0@users.noreply.github.com>
2025-07-26 22:17:31 +0300

    Add new font configurations for 'Markazi Text' and 'Mona Sans' in layout, enhancing typography options and improving overall design consistency.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,21 @@
-import { Inter, Roboto } from 'next/font/google'
+import { Inter, Roboto, Markazi_Text, Mona_Sans } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 const roboto = Roboto({ 
   weight: ['400', '700'],
   subsets: ['latin'] 
+})
+
+const markaziText = Markazi_Text({ 
+  weight: ['400', '500', '600', '700'],
+  subsets: ['arabic'],
+  variable: '--font-markazi-text'
+})
+
+const monaSans = Mona_Sans({ 
+  weight: ['200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ['latin'],
+  variable: '--font-mona-sans'
 })
 
 export default function RootLayout({
@@ -14,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Welcome to our website! We are dedicated to providing you with the best experience possible. Our website is designed to be user-friendly and easy to navigate. We hope you enjoy your time here!" />
         <meta name="keywords" content="website, design, development, SEO, marketing, branding, digital, agency, creative, services, solutions, technology, innovation, web, app, mobile, responsive, custom, responsive, design, development, marketing, branding, digital, agency, creative, services, solutions, technology, innovation, web, app, mobile, responsive, custom" />
         <meta name="author" content="Your Name" />
@@ -24,7 +36,7 @@ export default function RootLayout({
         <meta name="yandexbot" content="index, follow" />
         <meta name="sitemap" content="https://www.yourwebsite.com/sitemap.xml" /> 
         <meta name="google-site-verification" content="your-verification-code" /> 
-   
+
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -39,7 +51,7 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body className={`${inter.className} ${roboto.className}`} suppressHydrationWarning>
+      <body className={`${inter.className} ${roboto.className} ${markaziText.variable} ${monaSans.variable}`} suppressHydrationWarning>
         {children}
       </body>
     </html>
